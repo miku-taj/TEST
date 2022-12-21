2534a8a44f7f17266257c17a7648ab3c48d7de08
miku-taj <tajmukaddas@gmail.com>
2022-12-21 12:37:42 -0700

    Fix the issue
    worked a bit on dealing with commit issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 module.exports = {
   theme: {
     screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
+      xs: { max: "475px" },
 
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
 
       md: { max: "767px" },
       // => @media (max-width: 767px) { ... }
 
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
-      xs: { max: "550px" },
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
     },
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
   },
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
